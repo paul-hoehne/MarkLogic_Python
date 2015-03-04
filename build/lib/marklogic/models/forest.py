44555b08e98da32267b94c23b75bf2d7df3b0d52
Paul Hoehne <paul.hoehne@gmail.com>
2015-03-04 09:35:31 -0500

    Updates related to push to PyPi

---
+++ build/lib/marklogic/models/forest.py
@@ -0,0 +1,91 @@
+#
+# Copyright 2015 MarkLogic Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0#
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+__author__ = 'phoehne'
+
+import socket
+import requests
+import json
+
+class Forest:
+    def __init__(self, name):
+        self.config = {
+            u'forest-name': name,
+            u'host': socket.gethostname().lower()
+        }
+
+    def  set_host(self, host='localhost'):
+        self.config[u'host'] = host
+        return self
+
+    def set_database(self, db='Documents'):
+        self.config[u'database'] = db
+        return self
+
+    def set_data_directory(self, datadir='/var/opt/MarkLogic/Data'):
+        self.config[u'data-directory'] = datadir
+        return self
+
+    def set_large_data_directory(self, datadir='/var/opt/MarkLogic/Big_Data'):
+        self.config[u'large-data-directory'] = datadir
+        return self
+
+    def set_fast_data_directory(self, datadir='/var/opt/MarkLogic/Fast_Data'):
+        self.config[u'fast-data-directory'] = datadir
+        return self
+
+    def set_availability(self, which='online'):
+        self.config[u'availability'] = which
+        return self
+
+    def create(self, connection):
+        uri = "http://{0}:{1}/manage/v2/forests".format(connection.host, connection.management_port)
+        response = requests.post(uri, json=self.config, auth=connection.auth)
+        if response.status_code > 299:
+            raise Exception(response.text)
+
+        return self
+
+    def save(self, connection):
+        uri = "http://{0}:{1}/manage/v2/forests/{2}/properties".format(connection.host, connection.management_port,
+                                                              self.config[u'forest-name'])
+        response = requests.put(uri, json=self.config, auth=connection.auth)
+
+        if response.status_code > 299:
+            raise Exception(response.text)
+
+        return self
+
+    def remove(self, connection):
+        uri = "http://{0}:{1}/manage/v2/forests/{2}?level=full".format(connection.host, connection.management_port,
+                                                              self.config[u'forest-name'])
+        response = requests.delete(uri, auth=connection.auth)
+
+        if response.status_code > 299 and not response.status_code == 404:
+            raise Exception(response.text)
+
+        return self
+
+    @classmethod
+    def lookup(cls, name, connection):
+        uri = "http://{0}:{1}/manage/v2/forests/{2}/properties".format(connection.host, connection.management_port, name)
+        response = requests.get(uri, auth=connection.auth, headers={u'accept': u'application/json'})
+        if response.status_code > 299:
+            raise response
+        result = Forest("temp")
+        result.config = json.loads(response.text)
+
+        return result
---
+++ build/lib/marklogic/models/utilities/validators.py
@@ -0,0 +1,106 @@
+#
+# Copyright 2015 MarkLogic Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0#
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+"""
+Validators are utility functions used by various classes to validate
+input.
+"""
+
+class ValidationError(Exception):
+    def __init__(self, message, original_value):
+        self._message = message
+        self._original_value = original_value
+
+    def __repr__(self):
+        "Validation Error('{0}', {1})".format(self._message, self._original_value)
+
+
+def validate_boolean(raw_val):
+    if type(raw_val) != bool:
+        raise ValidationError('Value passed is not a boolean', repr(raw_val))
+
+
+def validate_index_type(raw_val):
+    valid_index_types = {"int", "unsignedInt", "long", "unsignedLong", "float", "double", "decimal", "dateTime",
+                         "time", "date",  "gYearMonth", "gYear", "gMonth", "gDay", "yearMonthDuration",
+                         "dayTimeDuration", "string", "anyURI"}
+    if raw_val not in valid_index_types:
+        raise ValidationError('Value is not a valid index type', repr(raw_val))
+
+
+def validate_index_invalid_value_actions(raw_val):
+    valid_actions = {'ignore', 'reject'}
+
+    if raw_val not in valid_actions:
+        raise ValidationError("Value is not a valid action for invalid index values", repr(raw_val))
+
+
+def validate_stemmed_searches_type(raw_val):
+    valid_types = {'off', 'basic', 'advanced', 'decompounding'}
+
+    if raw_val not in valid_types:
+        raise ValidationError("Stemmed search type is not a valid type of stemmed search", repr(raw_val))
+
+
+def validate_integer_range(raw_val, min, max):
+    if raw_val not in range(min, (1 + max)):
+        raise ValidationError("Integer value out of range", repr(raw_val))
+
+
+def validate_directory_creation(raw_val):
+    if raw_val not in ['manual', 'automatic', 'manual-enforced']:
+        raise ValidationError("Invalid directory creation method", repr(raw_val))
+
+
+def validate_locking_type(raw_val):
+    if raw_val not in ['strict', 'fast', 'off']:
+        raise ValidationError("Invalid locking option", repr(raw_val))
+
+
+def validate_range_index_optimize_options(raw_val):
+    if raw_val not in ['facet-time', 'memory-size']:
+        raise ValidationError("Range index optimize option is not a valid value", repr(raw_val))
+
+
+def validate_format_compatibility_options(raw_val):
+    if raw_val not in ['5.0', '4.2', '4.1', '4.0', '3.2']:
+        raise ValidationError("On disk index format comatibility objest is not a valide value", repr(raw_val))
+
+
+def validate_index_detection_options(raw_val):
+    if raw_val not in ['automatic', 'none']:
+        raise ValidationError("Index detection options is not a valid value", repr(raw_val))
+
+
+def validate_expunge_locks_options(raw_val):
+    if raw_val not in ['automatic', 'none']:
+        raise ValidationError("Expunge locks option is not a valid value", repr(raw_val))
+
+
+def validate_term_frequency_normalization_options(raw_val):
+    if raw_val not in ['unscaled-log', 'weakest-scaled-log', 'weakly-scaled-log', 'moderately-scaled-log',
+                      'strongly-scaled-log', 'scaled-log']:
+        raise ValidationError("Term frequency normalization option is not a valid value", repr(raw_val))
+
+
+def validate_merge_priority_options(raw_val):
+    if raw_val not in ['lower', 'normal']:
+        raise ValidationError("Merge priority option is not a valid value", repr(raw_val))
+
+
+def validate_assignment_policy_options(raw_val):
+    if raw_val not in ['bucket', 'statistical', 'range', 'legacy']:
+        raise ValidationError("Assignment policy option is not a valid value", repr(raw_val))
