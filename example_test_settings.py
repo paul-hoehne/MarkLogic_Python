ec849814cd65adf4a1af7bfd975ad32e96f4d16c
Paul Hoehne <paul.hoehne@gmail.com>
2015-04-10 12:41:51 -0400

    manually inserting changes

---
+++ example_test_settings.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals, print_function, absolute_import
+
+#
+# Copyright 2015 MarkLogic Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0#
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# File History
+# ------------
+#
+# Paul Hoehne       03/28/2015     Initial development
+#
+
+"""
+Edit this file to create test settings applicable to your system.
+The string must represent a complete path to an existing directory on the server.
+You will find the current root data directory on the ML Admin Forests Summary page.
+
+"""
+
+class DatabaseSettings(object):
+    large_data_directory = ""
+    fast_data_directory = ""
+
+
