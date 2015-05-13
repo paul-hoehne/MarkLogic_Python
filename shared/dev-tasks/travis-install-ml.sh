207be41bd4e1919cbe9a976bf3f709f44e25d168
Norman Walsh <ndw@nwalsh.com>
2015-05-12 19:56:00 -0500

    Switch to nightly builds; I've fixed bugs in the server

--- shared/dev-tasks/travis-install-ml.sh
+++ shared/dev-tasks/travis-install-ml.sh
@@ -58,7 +58,7 @@ else
   suff="_amd64.deb"
   fnamedeb=$fnamedeb$suff
 
-  url="http://root.marklogic.com/nightly/builds/linux64/rh6-intel64-80-test-1.marklogic.com/HEAD/pkgs.$day/$fname"
+  url="https://root.marklogic.com/nightly/builds/linux64/rh6-intel64-80-test-1.marklogic.com/b8_0/pkgs.$day/$fname"
 
   status=$(curl -k --anyauth -u $MLBUILD_USER:$MLBUILD_PASSWORD --head --write-out %{http_code} --silent --output /dev/null $url)
   if [[ $status = 200 ]]; then
